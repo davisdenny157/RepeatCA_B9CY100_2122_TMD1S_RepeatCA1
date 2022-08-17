78abf8f7863058b6f782750b41a95f65db1f2785
davisdenny157 <davisdenny157@gmail.com>
2022-08-17 21:16:21 +0100

    Second Commit

--- basket (1).py
+++ basket (1).py
@@ -12,4 +12,15 @@ class Basket:
         """ Item is a list of dictionaries which contains the items in basket """
         self.items = []
 
-    
+    def add_item(self, item_name, quantity):
+        """ This class method accepts item name and quantity 
+            and stores it in the items variable along with total price """
+        try:
+          if quantity < 1:
+            print("Error: Please enter a positive non zero quantity")
+          else:
+            price = self.price_list[item_name]
+            total_price = price * quantity
+            self.items.append({"item": item_name, "total_price": total_price})
+        except KeyError as e:
+          print("KeyError: ", e)
